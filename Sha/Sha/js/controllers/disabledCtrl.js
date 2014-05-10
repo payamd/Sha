580d66271f0af25d00a95633ad8e93df01c61d98
Payam Davoudi <payam.d7@gmail.com>
2014-05-10 10:51:59 -0800

    javaScript added to to from

---
+++ Sha/Sha/js/controllers/disabledCtrl.js
@@ -0,0 +1,36 @@
+﻿function f2nremoveDisable() {
+    document.getElementById("f2fn").disabled = false;
+    document.getElementById("f2ln").disabled = false;
+    document.getElementById("f2ma").disabled = false;
+    document.getElementById("f2fe").disabled = false;
+    document.getElementById("f2namesave").style.display = "";
+    document.getElementById("f2penn").style.display = "none";
+    
+}
+function f2naddDisable() {
+    document.getElementById("f2fn").disabled = true;
+    document.getElementById("f2ln").disabled = true;
+    document.getElementById("f2ma").disabled = true;
+    document.getElementById("f2fe").disabled = true;
+    document.getElementById("f2namesave").style.display = "none";
+    document.getElementById("f2penn").style.display = "";
+}
+
+
+
+function f2phremoveDisable() {
+    document.getElementById("f2pht").disabled = false;
+    document.getElementById("f2phn").disabled = false;
+    document.getElementById("f2phc").disabled = false;
+    document.getElementById("f2php").style.display = "";
+    document.getElementById("f2phsave").style.display = "";
+    document.getElementById("f2penph").style.display = "none";
+}
+function f2phaddDisable() {
+    document.getElementById("f2pht").disabled = true;
+    document.getElementById("f2phn").disabled = true;
+    document.getElementById("f2phc").disabled = true;
+    document.getElementById("f2php").style.display = "none";
+    document.getElementById("f2phsave").style.display = "none";
+    document.getElementById("f2penph").style.display = "";
+}
